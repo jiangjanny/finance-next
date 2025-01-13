5b0fcfd3f2d789ba6ed559395af7670251c7de9a
janny <yuaog1331@gmail.com>
2025-01-14 02:06:50 +0800

    d

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 // next.config.ts
 import type { NextConfig } from "next";
-
+import path from 'path';
 const nextConfig: NextConfig = {
   reactStrictMode: true, // 啟用 React 嚴格模式
   images: {
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // 加載環境變數
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname),
+    },
+  }
 };
 
 export default nextConfig;
